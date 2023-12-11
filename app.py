6cb94421adeb8a50d05901f9bc5800ef1488269d
jag-prabhakaran <73809351+jag-prabhakaran@users.noreply.github.com>
2023-12-11 14:39:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import tldextract
 from sklearn.base import BaseEstimator, TransformerMixin
 import requests
 import re
+import time
 
 app = Flask(__name__)
 
@@ -101,6 +102,8 @@ headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}, {response.text}")
     return response.json()
 
 def extract_urls(text):
@@ -112,14 +115,44 @@ def extract_urls(text):
 
     return urls
 
+def get_huggingface_output(text, retries=3):
+    for i in range(retries):
+        try:
+            output = query({
+                "inputs": text,
+                "parameters": {
+                    "truncation": "only_first"
+                }
+            })
+
+            if len(output) > 0 and isinstance(output[0], list):
+                return max(output[0], key=lambda x: x['score'])
+            else:
+                print(f"Unexpected output format. Retrying {i + 1}/{retries}...")
+        
+        except (KeyError, IndexError):
+            print(f"Error on attempt {i + 1}/{retries}. Retrying...")
+
+        except Exception as e:
+            print(f"Unexpected error: {e}. Retrying {i + 1}/{retries}...")
+    raise ValueError("Failed to get valid output after multiple attempts.")
+
+
 def get_predictions(url, text):
-    url_prediction, url_confidence = predict_url(clf, url)
+    if url != "None":  
+        url_prediction, url_confidence = predict_url(clf, url)
 
     url_list = extract_urls(text)
+    if len(url_list) < 1 and url == "None":
+        url_prediction = "None"
+        url_confidence = 0
+    
     goodCount = 0
     badCount = 0
-    for url in url_list:
-      prediction, confidence = predict_url(clf, url)
+    totConfidence = 0
+    for item in url_list:
+      prediction, confidence = predict_url(clf, item)
+      totConfidence += confidence
       if prediction == 'good':
         goodCount += 1
       else:
@@ -131,40 +164,45 @@ def get_predictions(url, text):
       url_pred = 'bad'
     elif goodCount == badCount == 0:
       url_pred = None
+    if len(url_list) != 0 and totConfidence != 0:
+        totConfidence = totConfidence / len(url_list)
 
     if url_pred != None:
       url_prediction = url_pred
-
+      url_confidence = totConfidence
+    print(url)
+    
+    if url == "None":
+        url_confidence = totConfidence
+    output = None
     # Text prediction
-    output = query({
-    "inputs": text,
-    "parameters": {
-      "truncation": "only_first"
-    }
-    })
+    max_confidence_label = get_huggingface_output(text)
 
-    max_confidence_label = max(output[0], key=lambda x: x['score'])
+    
+        
     predicted_label = "good" if max_confidence_label['label'] == 'LABEL_0' else "bad"
     text_confidence_score = max_confidence_label['score']
-
+    print("URL" + url_prediction, url_confidence, predicted_label, text_confidence_score)
     return url_prediction, url_confidence, predicted_label, text_confidence_score
 
 def ensemble_prediction(url, text):
     url_prediction, url_confidence, text_prediction, text_confidence = get_predictions(url, text)
     if url_prediction == "None":
-      print(url_prediction + " : " + str(url_confidence))
+      print("text" + text_prediction, text_confidence)
+      return text_prediction, text_confidence
     if text_prediction == "None":
-      print(text_prediction + " : " + str(text_confidence))
+      print(url_prediction, url_confidence)
+      return url_prediction, url_confidence
     if url_prediction == text_prediction:
         print(url_prediction, (url_confidence + text_confidence)/2)
         return url_prediction, (url_confidence + text_confidence)/2
     else:
         if url_confidence > text_confidence:
-            print(url_prediction, (url_confidence + text_confidence)/2)
-            return url_prediction, (url_confidence + text_confidence)/2
+            print(url_prediction, (50 + ((url_confidence * 100) / 4)))
+            return url_prediction, (50 + ((url_confidence * 100) / 4))
         else:
-            print(text_prediction, (url_confidence + text_confidence)/2)
-            return text_prediction, (url_confidence + text_confidence)/2
+            print(text_prediction, (50 + ((text_prediction * 100) / 4)))
+            return text_prediction, (50 + ((text_prediction * 100) / 4))
         
         
         
@@ -172,8 +210,25 @@ def ensemble_prediction(url, text):
 def form():
     email_body = request.form.get("email_body")
     embedded_url = request.form.get("url")
-    prediction, confidence = ensemble_prediction(embedded_url, email_body)
-    return render_template("form.html", email_body=email_body, embedded_url=embedded_url, prediction=prediction, confidence=int(confidence * 100))
+    
+    if not embedded_url:
+        embedded_url = "None"
+    
+    try:
+        prediction, confidence = ensemble_prediction(embedded_url, email_body)
+        return render_template("form.html", email_body=email_body, embedded_url=embedded_url, prediction=prediction, confidence=confidence)
+    
+    except ValueError as e:
+        # This will catch the "Failed to get valid output after multiple attempts." error and any other ValueErrors
+        if "Failed to get valid output after multiple attempts." in str(e):
+            return render_template("form.html", email_body=email_body, embedded_url=embedded_url, error="Failed to get a prediction. Please try again.")
+        else:
+            # Handle other ValueErrors differently if you like
+            return render_template("form.html", email_body=email_body, embedded_url=embedded_url, error=str(e))
+    
+    except Exception as e:
+        # This will catch any other unexpected exceptions and show a generic error message
+        return render_template("form.html", email_body=email_body, embedded_url=embedded_url, error="An unexpected error occurred. Please try again.")
 
 if __name__ == "__main__":
     app.run(debug=True)
